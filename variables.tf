fa88663d6af8e76d3606c04009397d41a068fcf1
Animesh Rana <animesh.rana@hpe.com>
2020-05-21 13:22:40 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,156 +1,28 @@
-variable "namespace" {
-  description = "Namespace (e.g. `pu` or `partsunlimited`)"
-  type        = string
+# AWS Region
+variable "region" {
+  default = "us-west-1"
 }
 
-variable "stage" {
-  description = "Stage (e.g. `prod`, `dev`, `staging`)"
-  type        = string
+# The AMI image to use (Debian 8)
+variable "aws_amis" {
+  default = {
+    "us-west-1"      = "ami-06fcc1f0bc2c8943f"
+  }
 }
 
-variable "name" {
-  type        = string
-  description = "Drupal"
+# Size of the instance
+variable "instance_type" {
+  default = "t2.small"
 }
 
-variable "delimiter" {
-  type        = string
-  default     = "-"
-  description = "Delimiter to be used between `namespace`, `stage`, `name` and `attributes`"
+# SSH key to deploy
+variable "key_name" {
+  description = "Key pair to use"
+  default     = "terraform_vm"
 }
 
-variable "attributes" {
-  type        = list(string)
-  default     = []
-  description = "Additional attributes (e.g. `policy` or `role`)"
-}
-
-variable "tags" {
-  type        = map(string)
-  default     = {}
-  description = "Additional tags (e.g. map(`BusinessUnit`,`XYZ`)"
-}
-
-variable "availability_zones" {
-  type        = list(string)
-  description = "List of Availability Zones (e.g. `['us-west-1a', 'us-west-1b', 'us-west-1c']`)"
-}
-
-variable "max_subnets" {
-  default     = "6"
-  description = "Maximum number of subnets that can be created. The variable is used for CIDR blocks calculation"
-}
-
-variable "type" {
-  type        = string
-  default     = "private"
-  description = "Type of subnets to create (`private` or `public`)"
-}
-
-variable "vpc_id" {
-  type        = string
-  description = "VPC ID"
-}
-
-variable "cidr_block" {
-  type        = string
-  description = "Base CIDR block which is divided into subnet CIDR blocks (e.g. `10.0.0.0/16`)"
-}
-
-variable "igw_id" {
-  type        = string
-  description = "Internet Gateway ID that is used as a default route when creating public subnets (e.g. `igw-9c26a123`)"
-  default     = ""
-}
-
-variable "az_ngw_ids" {
-  type        = map(string)
-  description = "Only for private subnets. Map of AZ names to NAT Gateway IDs that are used as default routes when creating private subnets"
-  default     = {}
-}
-
-variable "public_network_acl_id" {
-  type        = string
-  description = "Network ACL ID that is added to the public subnets. If empty, a new ACL will be created"
-  default     = ""
-}
-
-variable "private_network_acl_id" {
-  type        = string
-  description = "Network ACL ID that is added to the private subnets. If empty, a new ACL will be created"
-  default     = ""
-}
-
-variable "public_network_acl_egress" {
-  description = "Egress network ACL rules"
-  type        = list(map(string))
-
-  default = [
-    {
-      rule_no    = 100
-      action     = "allow"
-      cidr_block = "0.0.0.0/0"
-      from_port  = 0
-      to_port    = 0
-      protocol   = "-1"
-    },
-  ]
-}
-
-variable "public_network_acl_ingress" {
-  description = "Egress network ACL rules"
-  type        = list(map(string))
-
-  default = [
-    {
-      rule_no    = 100
-      action     = "allow"
-      cidr_block = "0.0.0.0/0"
-      from_port  = 0
-      to_port    = 0
-      protocol   = "-1"
-    },
-  ]
-}
-
-variable "private_network_acl_egress" {
-  description = "Egress network ACL rules"
-  type        = list(map(string))
-
-  default = [
-    {
-      rule_no    = 100
-      action     = "allow"
-      cidr_block = "0.0.0.0/0"
-      from_port  = 0
-      to_port    = 0
-      protocol   = "-1"
-    },
-  ]
-}
-
-variable "private_network_acl_ingress" {
-  description = "Egress network ACL rules"
-  type        = list(map(string))
-
-  default = [
-    {
-      rule_no    = 100
-      action     = "allow"
-      cidr_block = "0.0.0.0/0"
-      from_port  = 0
-      to_port    = 0
-      protocol   = "-1"
-    },
-  ]
-}
-
-variable "enabled" {
-  description = "Set to false to prevent the module from creating any resources"
-  default     = "true"
-}
-
-variable "nat_gateway_enabled" {
-  description = "Flag to enable/disable NAT Gateways creation in public subnets"
-  default     = "true"
+# Whitelist your IP for SSH access here
+variable "ip_whitelist" {
+  description = "Whitelisted IP for SSH access"
+  default     = "0.0.0.0/0"
 }
