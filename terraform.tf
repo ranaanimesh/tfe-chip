769ada5d9ea51966ac6bfc6b0b67a8c8987a6954
Animesh Rana <animesh.rana@hpe.com>
2020-05-21 14:43:08 -0500

    Create terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = "> 0.12.0"
+  required_providers {
+    aws = "~> 2.62"
+  }
+}
+
+provider "aws" {
+  alias   = "aws-west"
+  region  = "us-west-1"
+}
+
+provider "aws" {
+  alias   = "aws-eu"
+  region  = "eu-central-1"
+}
