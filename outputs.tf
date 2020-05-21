c87aec8e9fd5b653e1721e80fe34330ae2864f88
Animesh Rana <animesh.rana@hpe.com>
2020-05-21 13:09:11 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 # The EC2 instance
-output "EC2 IP address" {
+output "EC2_IP_address" {
   value = "${aws_instance.drupal.public_ip}"
 }
 
-output "EC2 DNS name" {
+output "EC2_DNS_name" {
   value = "${aws_instance.drupal.public_dns}"
 }
 
